c33c3db6f1c202af0af0ad3bb7f9be84b26c8526
nguyendangnfq <nguyen.dang@nfq.asia>
2022-07-29 10:20:29 +0700

    feat: learning navigation feature of nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -14,8 +15,13 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to{' '}
+          <a href="https://github.com/nguyendangnfq">Personal Page</a>
         </h1>
+
+        <Link href="/store">
+          <a>Store Page</a>
+        </Link>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -66,7 +72,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
